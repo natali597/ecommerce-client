536cf690b221d54098e6d7a8c8ec547469f36116
natali597 <natalinatali597@gmail.com>
2022-11-24 09:44:16 +0200

    fs

--- src/featuers/card.js
+++ src/featuers/card.js
@@ -7,11 +7,7 @@ export const cardlice = createSlice({
     add: (state, action) => {
       state.value = [...state.value, action.payload];
     },
-    // logout: (state) => {
-    //   state.value = { first_name: "", last_name: "", email: "" };
-    // },
   },
 });
 export const { add } = cardlice.actions;
-// export const { logout } = cardlice.actions;
 export default cardlice.reducer;
